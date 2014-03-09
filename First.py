b5098fa1fd9c8b612aeeea8b09443ab433c8b984
william <chobits5468@gmail.com>
2014-03-09 18:13:04 +0800

    [update] 將第一個例子加入超連結，能夠換頁

--- First.py
+++ First.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
+'''
+第一個練習，簡單的網頁連結
+'''
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<a href="#">index page!</a>'
+    return '<a href="/hello">index page!</a><br>' \
+    + '<a href="/hello">hello world page!</a><br>' \
+    + '<a href="/hello/ASDF">string hello world page!</a><br>' \
+    + '<a href="/hello/123">int hello world page!</a><br>' \
+    + '<a href="/hello/1.344">float hello world page!</a><br>'
 
 @app.route('/hello')
-@app.route('/hello/<name>')
 def hello_world(name=None):
     return render_template('hello.html', name=name)
 
@@ -24,6 +30,17 @@ def show_post(post_id):
 @app.route('/post/<float:var>')
 def show_float(var):
     return 'float %s' % var
+
+# @app.route('/asdf/')
+# def show_test():
+#		'''接受/asdf和/asdf/'''
+#     return 'test'
+
+# @app.route('/asdf')
+# def show_test3():
+#		'''不接受/asdf/'''
+#     return 'test1'
+
 
 if __name__ == '__main__':
     app.run(debug=True)    
